6cdb05de6db668226860ec989d88876c80a93d54
Inisepp <Septian893@gmail.com>
2021-08-07 14:07:37 +0700

    backend-buku
    pustaka buku

---
+++ application/views/view-latihan.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Latihan 1</title>
+    </head>
+    <body>
+        Halo kawann... Yuk Kita belajar Web Programming!!<br>
+        Nilai 1 = <?= $nilai1; ?><br>
+        Nilai 2 = <?= $nilai2; ?><br>
+        <br>Ini Hasil dari pemodelan penjumlahan dengan metode penjumlahan yaitu <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+
+    </body>
+</html>
